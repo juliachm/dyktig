03f9ca7d1427ff600a3fdb116b6f5aa35d3f8931
Julia Chmurna <juliachm@uio.no>
2023-07-02 22:28:07 +0200

    Remove unnecessary file

--- vite.config.js.timestamp-1686636875257-054306de69b2f.mjs
+++
@@ -1,7 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'file:///C:/Users/jchmu/OneDrive/Desktop/dyktig-app/node_modules/vite/dist/node/index.js';
-import react from 'file:///C:/Users/jchmu/OneDrive/Desktop/dyktig-app/node_modules/@vitejs/plugin-react/dist/index.mjs';
-var vite_config_default = defineConfig({
-  plugins: [react()],
-});
-export { vite_config_default as default };
